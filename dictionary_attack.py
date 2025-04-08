d376277a4f845bae0cad439975e119059bda6a4a
Geekuer <120203378+Geekuer@users.noreply.github.com>
2025-04-08 21:53:49 +0900

    Add files via upload

--- dictionary_attack.py
+++ dictionary_attack.py
@@ -1,6 +1,7 @@
 import requests
 
 url = "https://b47bda01-bed6-420e-91a1-8bd138fd806c-00-13yryay7g98u4.sisko.replit.dev/login"
+# url = "http://127.0.0.1:5000/login"
 username = "admin"
 
 # Read password list from text file
