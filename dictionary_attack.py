92ea9db1c492fd1828ff8c84b545962dae2a5555
Geekuer <120203378+Geekuer@users.noreply.github.com>
2025-05-04 14:38:38 +0900

    dictionary_attack.py 업데이트

--- dictionary_attack.py
+++ dictionary_attack.py
@@ -1,7 +1,6 @@
 import requests
 
-url = "https://b47bda01-bed6-420e-91a1-8bd138fd806c-00-13yryay7g98u4.sisko.replit.dev/login"
-# url = "http://127.0.0.1:5000/login"
+url = "http://127.0.0.1:5000/login"
 username = "admin"
 
 # Read password list from text file
